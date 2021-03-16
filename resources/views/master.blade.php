892defd34990b58cf37f7ab5395e3217e960394e
hisyamhawari_ <hawari.hisyam@gmail.com>
2021-03-16 08:36:57 +0700

    adding some component

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -50,13 +50,9 @@
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
+    <!-- Brand -->
     <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png"
-           alt="AdminLTE Logo"
-           class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="../Logo/Frame 41.png">
     </a>
 
     <!-- Sidebar -->
@@ -252,7 +248,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a href="../examples/blank.html" class="nav-link active">
+                <a href="../examples/blank.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Blank Page</p>
                 </a>
