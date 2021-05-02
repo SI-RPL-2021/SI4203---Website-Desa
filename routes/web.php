896fd2a697b9c98838531ae13d0abc79559c00a6
hisyamhawari_ <hawari.hisyam@gmail.com>
2021-05-02 14:15:25 +0700

    Coba

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,9 @@ Route::get('/contact', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/', 'ArticleController@home')->name("article.home");
+Route::post('/article', 'ArticleController@create')->name("article.create");
+Route::get('/article', function () {
+    return view('article');
+})->name("article");
+Route::get('/article/{id}', 'ArticleController@detail')->name("article.detail");
