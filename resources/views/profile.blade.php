d1a12de9c325ec72021328a629fddb644d859722
annisanyuns <nuannisanyun1600@gmail.com>
2021-05-09 23:47:16 +0700

    update sejarah desa  #5

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,6 +10,6 @@ Sejarah Desa
         <div class="col-12">
           <div class="card">
           <div class="card-header">
-          <img class="card-img-top" src="https://via.placeholder.com/750x300" alt="Card image cap" />
+          <img class="card-img-top" src="https://via.placeholder.com/1000x200" alt="Card image cap" 
           <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
           @endsection
