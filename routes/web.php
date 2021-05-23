9bc45f5710564ab1e4e9b88c4766704e2b03a514
Desyft <desyfitriani234@gmail.com>
2021-05-23 09:50:55 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ Route::get('/keuangan', function () {
     return view('keuangan');
 });
 
-<<<<<<< HEAD
+
 Route::get('/profile', function () {
     return view('profile');
-=======
+});
+
 Route::get('/kesehatan', function () {
     return view('kesehatan');
->>>>>>> Hisyam
+
 });
 
 Auth::routes();
@@ -31,14 +32,10 @@ Route::post('/article', 'ArticleController@create')->name("article.create");
 Route::get('/article', function () {
     return view('article');
 })->name("article");
-<<<<<<< HEAD
+
 
 Route::get('/lembaga', 'lembagaController@index')->name('lembaga');
-=======
-<<<<<<< HEAD
-=======
+
 Route::get('/article/{id}', 'ArticleController@detail')->name("article.detail");
 
 Route::resource('kesehatan', 'kesehatanController');
->>>>>>> Hisyam
->>>>>>> c4f133709455af2da2feb41aafc2738fb9931921
