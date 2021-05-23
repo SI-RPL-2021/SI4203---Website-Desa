78176236d2af6360655275d57914edf95bb2b9ca
hisyamhawari_ <hawari.hisyam@gmail.com>
2021-05-23 10:19:50 +0700

    fixed rout

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,17 @@ Route::get('/keuangan', function () {
     return view('keuangan');
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> ee0747385ec5d00a9ddc75bc6a3780d0fbdb2ce1
 Route::get('/profile', function () {
     return view('profile');
 });
 
-<<<<<<< HEAD
+
 Route::get('/aboutUs', function () {
     return view('aboutUs');
-=======
+});
+
 Route::get('/kesehatan', function () {
     return view('kesehatan');
-
->>>>>>> ee0747385ec5d00a9ddc75bc6a3780d0fbdb2ce1
 });
 
 Auth::routes();
@@ -41,15 +36,13 @@ Route::get('/article', function () {
     return view('article');
 })->name("article");
 
-<<<<<<< HEAD
+
 Route::get('/article/{id}', 'ArticleController@detail')->name("article.detail");
 
 Route::resource('kesehatan', 'kesehatanController');
-=======
 
 Route::get('/lembaga', 'lembagaController@index')->name('lembaga');
 
 Route::get('/article/{id}', 'ArticleController@detail')->name("article.detail");
 
 Route::resource('kesehatan', 'kesehatanController');
->>>>>>> ee0747385ec5d00a9ddc75bc6a3780d0fbdb2ce1
